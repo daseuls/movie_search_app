7027c5848bb889827e1f1810e9b60dd968dd3cf1
daseuls <dls2tmfs2@naver.com>
2022-05-09 16:42:25 +0900

    feat: styled component routes에 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Router from "./routes";
+import Router from "./pages/routes";
+import GlobalStyle from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
   </React.StrictMode>
 );
