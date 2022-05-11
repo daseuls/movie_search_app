a255e1f340f8e0e1359d51c93b0e2d8971133356
daseuls <dls2tmfs2@naver.com>
2022-05-11 10:13:38 +0900

    feat: recoil state 생성

--- src/components/SearcBar.tsx
+++ src/components/SearcBar.tsx
@@ -1,7 +1,30 @@
 import styled from "styled-components";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <Container>
+      <SearchBarForm>
+        <SearchBarInput placeholder="Search" />
+        <SearchBtn />
+      </SearchBarForm>
+    </Container>
+  );
 };
 
 export default SearchBar;
+
+const Container = styled.nav`
+  margin: 3rem 0 2rem;
+  position: "sticky";
+`;
+
+const SearchBarForm = styled.form``;
+
+const SearchBarInput = styled.input`
+  border: 1px solid black;
+  border-radius: 3rem;
+  width: 100%;
+  padding: 0.7rem;
+`;
+
+const SearchBtn = styled.button``;
---
+++ src/recoil/state.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+const movieListState = atom({
+  key: "#movieListState",
+  default: [],
+});
